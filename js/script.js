fbf0b52a0d9b1d393a32449fc8ebdfae78444998
MALIK6882 <85858285+MALIK6882@users.noreply.github.com>
2025-02-28 12:58:59 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ const config = {
             {
                 inGameName: "McDev",
                 rank: "Founder",
-                skinUrlOrPathToFile: "../images/mcdev.png",
+                skinUrlOrPathToFile: "../images/mcdev.jpg",
                 rankColor: "rgba(255, 3, 3, 1)"
             },
             {
